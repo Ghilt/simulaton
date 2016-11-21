6d3df889178b26cc538b60438e2e53a1b2f96fdf
Adam Nilsson <adam.n.a3@gmail.com>
2016-11-21 20:10:43 +0100

    Generalize linear transform

--- Simulaton/Mechanics/ValueTransformFunctions/LinearTransform.cs
+++ Simulaton/Mechanics/ValueTransformFunctions/LinearTransform.cs
@@ -8,40 +8,53 @@ namespace Simulaton.Mechanics.ValueTransformFunctions
 {
     class LinearTransform : TransformFunction<float, float>
     {
+        SortedList<float, Func<float, float>> points = new SortedList<float, Func<float, float>>();
 
-        SortedList<float, float> points = new SortedList<float, float>();
+        public LinearTransform(params float[] evenlySpacedValues)
+        {
+            int pointCount = evenlySpacedValues.Count();
+            KeyValuePair<float, float>[] pointList = new KeyValuePair<float, float>[pointCount];
+            float keyDistance = 1.0f / pointCount;
+            float pointKey = 0;
+            for (int i = 0; i < pointCount - 1; i++)
+            {
+                pointList[i] = new KeyValuePair<float, float>(pointKey, evenlySpacedValues[i]);
+                pointKey += keyDistance;
+            }
+            pointList[pointCount - 1] = new KeyValuePair<float, float>(1, evenlySpacedValues[pointCount - 1]);
+            ConvertPointsToFunctions(pointList);
+        }
 
         public LinearTransform(params KeyValuePair<float, float>[] pointList)
-        //: base(x => valueAtZero + x * (valueAtOne - valueAtZero))
         {
-            Array.ForEach(pointList, point => this.points.Add(point.Key, point.Value));
+            if (pointList.First().Key != 0) throw new ArgumentException("First point must be (0,y)");
+            if (pointList.Last().Key != 1) throw new ArgumentException("Last point must be (1,y)");
+            ConvertPointsToFunctions(pointList);
+        }
 
+        private void ConvertPointsToFunctions(KeyValuePair<float, float>[] pointList)
+        {
+            for (int i = 0; i < pointList.Count() - 1; i++)
+            {
+                float differenceY = (pointList[i + 1].Value - pointList[i].Value);
+                float differenceX = (pointList[i + 1].Key - pointList[i].Key);
+                if (differenceX == 0) throw new ArgumentException("Stepfunction not supported, X(n) and X(n+1) equal");
+                float slope = differenceY / differenceX;
+                points.Add(pointList[i].Key, x => slope * (x - pointList[i].Key) + pointList[i].Value);
+            }
         }
 
         public float Transform(float x)
         {
-
             float result = x;
-            float keyBefore = 0;
-            float keyAfter = 0;
-            foreach (float key in points.Keys)
+            for (int i = points.Count; i >= 0; i--)
             {
-                if (x <= key)
+                if (x >= points.Keys[i])
                 {
-                    keyAfter = key;
-                    break;
+                    return points[points.Keys[i]](x);
                 }
-                keyBefore = key;
-                keyAfter = key;
             }
-            float valueBefore, valueAfter;
-            points.TryGetValue(keyBefore, out valueBefore);
-            points.TryGetValue(keyAfter, out valueAfter);
-
-            float slope = (valueAfter - valueBefore)/(keyAfter - keyBefore);
-            result = slope*(x - keyBefore) + valueBefore;
-
-            return result;
+            return 0;
         }
     }
 }
