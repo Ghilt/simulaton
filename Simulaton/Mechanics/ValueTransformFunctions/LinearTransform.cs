59ce932f7aae45728316d0456da5918afcaca601
Ghilt <adam.n.a3@gmail.com>
2016-11-21 16:49:26 +0100

    First step of generalization of linearTransform

--- Simulaton/Mechanics/ValueTransformFunctions/LinearTransform.cs
+++ Simulaton/Mechanics/ValueTransformFunctions/LinearTransform.cs
@@ -6,14 +6,42 @@ using System.Threading.Tasks;
 
 namespace Simulaton.Mechanics.ValueTransformFunctions
 {
-    class LinearTransform : GenericTransform
+    class LinearTransform : TransformFunction<float, float>
     {
 
-        public LinearTransform(float valueAtZero, float valueAtOne)
-            : base(x => valueAtZero + x * (valueAtOne - valueAtZero))
+        SortedList<float, float> points = new SortedList<float, float>();
+
+        public LinearTransform(params KeyValuePair<float, float>[] pointList)
+        //: base(x => valueAtZero + x * (valueAtOne - valueAtZero))
         {
-            
+            Array.ForEach(pointList, point => this.points.Add(point.Key, point.Value));
+
         }
 
+        public float Transform(float x)
+        {
+
+            float result = x;
+            float keyBefore = 0;
+            float keyAfter = 0;
+            foreach (float key in points.Keys)
+            {
+                if (x <= key)
+                {
+                    keyAfter = key;
+                    break;
+                }
+                keyBefore = key;
+                keyAfter = key;
+            }
+            float valueBefore, valueAfter;
+            points.TryGetValue(keyBefore, out valueBefore);
+            points.TryGetValue(keyAfter, out valueAfter);
+
+            float slope = (valueAfter - valueBefore)/(keyAfter - keyBefore);
+            result = slope*(x - keyBefore) + valueBefore;
+
+            return result;
+        }
     }
 }
