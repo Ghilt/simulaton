3591f6ef9682321fc0f35fb4d4726dadda58880d
Ghilt <adam.n.a3@gmail.com>
2016-11-21 13:42:19 +0100

    Added simple linear transform

---
+++ Simulaton/Mechanics/ValueTransformFunctions/LinearTransform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulaton.Mechanics.ValueTransformFunctions
+{
+    class LinearTransform : GenericTransform
+    {
+
+        public LinearTransform(float valueAtZero, float valueAtOne)
+            : base(x => valueAtZero + x * (valueAtOne - valueAtZero))
+        {
+            
+        }
+
+    }
+}
